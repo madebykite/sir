4bc12bc23914ffbe0faefcf0b5c03d4ceea8b01f
Matthew Glubb <matthew.glubb@madebykite.com>
2025-05-20 15:22:10 +0100

    MBZ-375 Regenerated amqp trigger and function sql

--- sql/CreateAMQPTrigger.sql
+++ sql/CreateAMQPTrigger.sql
@@ -6,11 +6,3 @@ CREATE TRIGGER search_message_insert BEFORE INSERT ON sir.message
     FOR EACH ROW EXECUTE PROCEDURE search_message_insert();
 
 COMMIT;
--- Automatically generated, do not edit!
-\set ON_ERROR_STOP 1
-BEGIN;
-
-CREATE TRIGGER search_message_insert BEFORE INSERT ON sir.message
-    FOR EACH ROW EXECUTE PROCEDURE search_message_insert();
-
-COMMIT;
